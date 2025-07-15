72933df38be3e832fbbcbec6b6fe762942fa746a
Kire Smilkov <kiresmilkov14@gmail.com>
2025-07-15 16:33:10 +0200

    Register/Login/JWTToken

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,15 +1,12 @@
-using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")] // localhost:5001/api/members
-    [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+    public class MembersController(AppDbContext context) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<List<AppUser>>> GetMembers()
@@ -17,6 +14,7 @@ namespace API.Controllers
             var members = await context.Users.ToListAsync();
             return members;
         }
+        [Authorize]
         [HttpGet("{id}")] // localhost:5001/api/members/{id}
         public async Task<ActionResult<AppUser>> GetMember(string id)
         {
