45d284e052717259af49d07bd843e7c68ef90fa6
Kire Smilkov <kiresmilkov14@gmail.com>
2025-07-21 16:01:29 +0200

    EntityRelationships/Seeding/RepositoryPattern

---
+++ API/Interfaces/IMemberRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using API.Entities;
+
+namespace API.Interfaces;
+
+public interface IMemberRepository
+{
+    void Update(Member member);
+    Task<bool> SaveALLAsync();
+    Task<IReadOnlyList<Member>> GetMemberAsync();
+    Task<Member?> GetMemerByIdAsync(string id);
+    Task<IReadOnlyList<Photo>> GetPhotoForMemberAsync(string memberId);
+}
