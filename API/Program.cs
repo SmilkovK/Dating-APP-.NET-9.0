06ffe921a13eaf01d386fcdd56a2e85c7b6cb3b2
Kire Smilkov <kiresmilkov14@gmail.com>
2025-08-19 15:28:51 +0200

    fixing sql start

--- API/Program.cs
+++ API/Program.cs
@@ -18,7 +18,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(10),
+                errorNumbersToAdd: null 
+            );
+        });
 });
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
